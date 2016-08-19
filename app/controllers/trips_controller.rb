51e698954d1533333664ceea5505eecc835299d7
Ann Hall <anniemaesato@gmail.com>
2016-08-19 14:05:00 -0600

    trip controller edits

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,49 @@
 class TripsController < ApplicationController
+  before_action :trip, exept: [:index, :new, :create]
+  
   def index
+    @trip = Trip.all.by_date
   end
 
   def show
   end
 
   def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip)
+    else
+      rener :edit
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+
+    def trip_params
+      params.require(:trip).permit(:name, :begin, :end, :people, :notes)
+    end
+
+    def trip
+      @trip = Trip.find(params[:id])
+    end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ApplicationRecord
 	has_many :locations, dependent: :destroy
 
 	validates_presence_of :name
+
+	def self.by_date
+		order(:begin)
+	end
 end
