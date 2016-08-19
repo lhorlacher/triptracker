29e8c51b19d8b1ea99778fd0dfd6389754caceed
Andrew Biddle <abidds2@gmail.com>
2016-08-19 14:14:35 -0600

    updates

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,10 +1,67 @@
 class AddressesController < ApplicationController
+
+  def index
+    @address = address.all
+  end
+
+
   def show
+    @address =  @location.adress
   end
 
   def new
+    @address = Address.new
+  end
+
+
+  def create
+
   end
 
   def edit
+    @address = @trip.location.address.find(params[:id])
   end
+
+
+  def update
+    if @address = @trip.location.address.find(params[:id])
+      redirect_to trip_location_address_path
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @address.destroy
+    redirect_to trip_location_address_path
+  end
+
+
+
+  private
+
+  def address
+    @address = @trip.location.address.find(params[:id])
+  end
+
+  def loction
+    @location = @trip.location.find(params[:id])
+  end
+
+  def trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def address_params
+    params.require(:address).permit(:street, :city, :state, :country, :zip)
+  end
+
+
+
+
+
+
+
+
 end
