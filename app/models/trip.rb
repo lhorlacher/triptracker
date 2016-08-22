bc991d8b9b5dea705bfe1eb2bbe555dbd0fd8f23
Ann Hall <anniemaesato@gmail.com>
2016-08-22 14:21:44 -0600

    trip image

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,6 +4,9 @@ class Trip < ApplicationRecord
 
 	validates_presence_of :name
 
+	has_attached_file :image, styles: { small: ["300x300#", :jpg] }
+	validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+	
 	def self.by_date
 		order(:begin)
 	end
